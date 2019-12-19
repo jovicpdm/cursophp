dfaa155c1f245d6d6d6c84586ad4f3c855e963b9
João Victor Morais <jovic.morais@unifesspa.edu.br>
2019-12-19 11:29:43 -0300

    fa

---
+++ basico/html.php
@@ -0,0 +1,16 @@
+<div class="titulo">Integração HTML</div>
+
+<h1>
+    <?php
+    echo 'Olá';
+    echo '<small>';
+    echo ' Mundo!';
+    echo '</small>';
+    ?>
+</h1>
+
+<?= "<div>Outra Forma De Me 'Expressar'!</div>" ?>
+
+<br>
+
+<div><button><?= "Legal" ?></button></div>
