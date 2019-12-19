d047e0ddca9b852b377e7dcedbabbab89c48996a
João Victor Morais <jovic.morais@unifesspa.edu.br>
2019-12-19 11:15:26 -0300

    Continuação

---
+++ basico/ola.php
@@ -0,0 +1,21 @@
+<div class="titulo">
+
+<h2>Exemplo A</h2>
+
+<?php
+echo 'Olá <br>';
+echo 'Mundo'
+?>
+</div>
+
+<h2>Exemplo B</h2>
+
+<?=
+    "Outra forma de me 'expressar'"
+?>
+<br>
+<?=
+    45 * 35
+?>
+
+<?php
--- exercicio.php
+++ exercicio.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
@@ -7,20 +8,20 @@
     <link rel="stylesheet" href="recursos/css/exercicio.css">
     <title>Exercício</title>
 </head>
+
 <body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
-            class="verde">Sem formatação</a>
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem formatação</a>
         <a href="index.php" class="vermelho">Voltar</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
             <?php
-                include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
+            include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
             ?>
         </div>
     </main>
@@ -28,4 +29,5 @@
         COD3R & ALUNOS © <?= date('Y'); ?>
     </footer>
 </body>
+
 </html>
