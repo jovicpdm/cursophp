e12dcddb7f340a522deffb60488af0868b64543f
João Victor Morais <jovic.morais@unifesspa.edu.br>
2019-12-23 09:34:26 -0300

    Tipos Inteiro e Float

--- index.php
+++ index.php
@@ -44,6 +44,51 @@
                         </li>
                     </ul>
                 </div>
+                <div class="modulo vermelho">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=int">
+                                Tipo Inteiro
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=float">
+                                Tipo Float
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=aritmeticas">
+                                Op. Aritméticas
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=desafio_precedencia">
+                                Desafio Precedência
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=string">
+                                Tipo String
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=desafio_string">
+                                Desafio String
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=booleano">
+                                Tipo Booleano
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=tipos&file=conversoes">
+                                Conversões
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
     </main>
---
+++ tipos/int.php
@@ -0,0 +1,14 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11);
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+echo 017, '<br>'; //base octal
+echo 0b101001011001, '<br>'; //base binária
+echo 0x117ade; //base hexadecimal 
