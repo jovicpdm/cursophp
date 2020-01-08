c5a1d94887febd6a710f1c5e927a6bc39d699895
João Victor Morais <jovic.morais@unifesspa.edu.br>
2020-01-08 17:06:57 -0300

    Fim do Capítulo Tratamento de Erros

--- menu.php
+++ menu.php
@@ -1,4 +1,14 @@
 <nav class="modulos">
+    <div class="modulo vermelho">
+        <h3>12. Sessão</h3>
+        <ul>
+            <li>
+                <a href="exercicio.php?dir=sessao&file=basico_sessao">
+                    Sessão
+                </a>
+            </li>
+        </ul>
+    </div>
     <div class="modulo verde">
         <h3>11. Trat. de Erro</h3>
         <ul>
---
+++ sessao/basico_sessao_alterar.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+print_r($_SESSION);
+?>
+
+<p>
+    <b>Nome: </b> <?= $_SESSION['nome'] ?><br>
+    <b>Email: </b> <?= $_SESSION['email'] ?>
+</p>
+
+<?php
+$_SESSION['email'] = 'gabrielfilho_alt@emailaz.com.br';
+?>
+
+<p>
+    <a href="basico_sessao.php">Voltar</a>
+</p>
+<p>
+    <a href="basico_sessao_limpar.php">Limpar Sessão</a>
+</p>
